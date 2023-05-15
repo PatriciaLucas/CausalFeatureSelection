3b632a10411df0db90b96513ef61b5e1568514ca
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2023-05-15 13:12:21 -0300

    Update models_functions.py

--- models_functions.py
+++ models_functions.py
@@ -8,6 +8,16 @@ from sklearn.metrics import mean_squared_error
 import math
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import tensorflow as tf
+from keras.layers import Dense, Flatten, SpatialDropout1D, Activation, Add, BatchNormalization, LSTM
+from keras.models import Input, Model
+from keras.engine.topology import Layer
+from keras.constraints import unit_norm
+from keras import regularizers
+from keras.optimizers import Adam, SGD
+from keras.models import Sequential
+from keras.callbacks import EarlyStopping
+
 
 
 
@@ -18,8 +28,39 @@ def fit_LR(X_train, y_train):
 def predict_LR(X_test, y_test, model, target):
   yhat = model.predict(X_test)
   rmse = math.sqrt(mean_squared_error(y_test,yhat))
-  maxmin = np.max(y_test) - np.min(y_test)
-  nrmse = rmse/maxmin
+  #maxmin = np.max(y_test) - np.min(y_test)
+  nrmse = rmse/np.std(y_test)
+  return yhat, rmse, nrmse
+
+
+def fit_LSTM(X_train, y_train):
+    individual = {
+        'num_layers': 1,
+        'units': 72,
+        'dropout': 0.3,
+        'norm': 0
+        } 
+    model = Sequential()
+    call = [EarlyStopping(monitor='loss', mode='min', patience=15, verbose=1),]
+    for i in range(individual['num_layers']):
+        if i != individual['num_layers']:
+          model.add(LSTM(units=individual['units'], activation='relu', input_shape=(X_train.shape[1],1), return_sequences=True))
+        else:
+          model.add(LSTM(units=individual['units'], activation='relu', input_shape=(X_train.shape[1],1), return_sequences=False))
+        model.add(SpatialDropout1D(round(individual['dropout'],1)))
+        if individual['norm'] == 0:
+          model.add(BatchNormalization())
+    model.add(Flatten())
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='Adam')
+    model.fit(X_train, y_train, epochs = 100, verbose=0, batch_size=32, callbacks = call)
+    return model
+
+def predict_LSTM(X_test, y_test, model, target):
+  yhat = model.predict(X_test)
+  rmse = math.sqrt(mean_squared_error(y_test,yhat))
+  #maxmin = np.max(y_test) - np.min(y_test)
+  nrmse = rmse/np.std(y_test)
   return yhat, rmse, nrmse
 
 
