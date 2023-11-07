c9a5c6c255e6585f01e4b81959097e892e57714f
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2023-11-07 12:40:58 -0300

    Update models_functions.py

--- models_functions.py
+++ models_functions.py
@@ -4,65 +4,41 @@ Created on Wed Mar 29 11:12:30 2023
 
 @author: Patricia
 """
-from sklearn.metrics import mean_squared_error
+import pandas as pd
 import math
 import numpy as np
-from sklearn.linear_model import LinearRegression
-import tensorflow as tf
-from tensorflow import keras
-from keras.models import Sequential, Model
-from keras.constraints import max_norm, unit_norm
-from keras.layers import Dense, Flatten, SpatialDropout1D, Activation, Add, BatchNormalization, LSTM, Input
-from keras import regularizers
-from keras.optimizers import Adam, SGD
-from keras.callbacks import EarlyStopping
+from sklearn.model_selection import GridSearchCV
+from sklearn import ensemble
+from sklearn.metrics import mean_squared_error
+
+def gridsearch_RF(X, Y):
+    params = {
+     'bootstrap': [True],
+     'min_samples_leaf': [5,10,15,20,25,30],
+     'n_estimators': [10,60,110,160,210,260,310,360,410,460,510],
+     'max_features': ['sqrt', 'log2'],
+     }
+    model = GridSearchCV(estimator = ensemble.RandomForestRegressor(), param_grid = params, 
+                           cv = 3, n_jobs = -1, verbose = 0)
+    grid_result = model.fit(X, Y)
+    best_params = grid_result.best_params_
+    return best_params
 
 
-
-
-def fit_LR(X_train, y_train):
-  model = LinearRegression().fit(X_train, y_train)
-  return model
-
-def predict_LR(X_test, y_test, model, target):
-  yhat = model.predict(X_test)
-  rmse = math.sqrt(mean_squared_error(y_test,yhat))
-  #maxmin = np.max(y_test) - np.min(y_test)
-  nrmse = rmse/np.std(y_test)
-  return yhat, rmse, nrmse
-
-
-def fit_LSTM(X_train, y_train):
-    individual = {
-        'num_layers': 1,
-        'units': 72,
-        'dropout': 0.3,
-        'norm': 0
-        } 
-    model = Sequential()
-    call = [EarlyStopping(monitor='loss', mode='min', patience=15, verbose=1),]
-    for i in range(individual['num_layers']):
-        if i != individual['num_layers']:
-          model.add(LSTM(units=individual['units'], activation='relu', input_shape=(X_train.shape[1],1), return_sequences=True))
-        else:
-          model.add(LSTM(units=individual['units'], activation='relu', input_shape=(X_train.shape[1],1), return_sequences=False))
-        model.add(SpatialDropout1D(round(individual['dropout'],1)))
-        if individual['norm'] == 0:
-          model.add(BatchNormalization())
-    model.add(Flatten())
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='Adam')
-    model.fit(X_train, y_train, epochs = 100, verbose=0, batch_size=32, callbacks = call)
+def fit_RF(X_train, y_train, best_params):
+    
+    model = ensemble.RandomForestRegressor(n_estimators=best_params['n_estimators'], 
+                                           min_samples_leaf=best_params['min_samples_leaf'], 
+                                           random_state=0)
+    model.fit(X_train, y_train)
     return model
 
-def predict_LSTM(X_test, y_test, model, target):
+def predict_RF(X_test, y_test, model, step_ahead):
   yhat = model.predict(X_test)
   rmse = math.sqrt(mean_squared_error(y_test,yhat))
-  #maxmin = np.max(y_test) - np.min(y_test)
-  nrmse = rmse/np.std(y_test)
-  return yhat, rmse, nrmse
-
-
+  maxmin = np.max(y_test) - np.min(y_test)
+  nrmse = rmse/maxmin
+  return yhat, nrmse
 
 
 
